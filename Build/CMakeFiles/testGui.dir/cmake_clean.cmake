323a7502f6cbf4c815a8af70feb880339394e02a
yura-galoyan <galoyanyur@gmail.com>
2023-12-30 00:07:11 +0400

    cmake configured

---
+++ Build/CMakeFiles/testGui.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testGui.dir/test.cpp.obj"
+  "CMakeFiles/testGui.dir/test.cpp.obj.d"
+  "libtestGui.dll.a"
+  "testGui.exe"
+  "testGui.exe.manifest"
+  "testGui.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/testGui.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Build/CMakeFiles/testGui.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for testGui.
+# This may be replaced when dependencies are built.
